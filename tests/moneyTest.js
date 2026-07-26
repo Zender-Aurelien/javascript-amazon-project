ff4a76ce7da14cc1262c05fdac7aeca1e267a394
Zender <zenderaurelien4@gmail.com>
2026-07-25 20:45:12 -0400

    added test cases for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,24 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+console.log('test_suite: formatCurrency');
+
+console.log('convert cents into dollars');
+if(formatCurrency(2095) ==='20.95'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+console.log('converts 0 into dollar format');
+if(formatCurrency(0)==='0.00'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+
+console.log('rounds up');
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('passed');
+}else{
+    console.log('fail');
+}
