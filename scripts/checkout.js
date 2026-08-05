e4acc43b40d749e654043d292b797fe3b62feeaf
Zender <zenderaurelien4@gmail.com>
2026-08-05 00:27:42 -0400

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,60 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
-loadProducts(() =>{
+//promises are a better alternative to callbacks as they allow for less nesting (both let you run async code)
+
+//promise.all lets you run multiple promises and wait for all to finish
+Promise.all([
+    new Promise((resolve) =>{ 
+        loadProducts(() =>{
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) =>{
+        loadCart(() =>{
+            resolve();
+        });
+    })
+
+
+]).then((values) =>{
+    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
 });
+
+
+
+
+/*
+new Promise((resolve) =>{ // resolve function that controls when to move to next step
+    loadProducts(() =>{
+        resolve('value1');//after loadProducts is loaded, resolve() allowes it to move onto the next step, then()
+    });//promises allow for threading(?)
+
+}).then((value) =>{//calls the rest of the code
+    return new Promise((resolve) =>{
+        loadCart(() =>{
+            resolve();
+        });
+    });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+})*/
+
+
+/*
+loadProducts(() =>{
+    loadCart(() =>{
+        renderOrderSummary();
+        renderPaymentSummary();
+        renderCheckoutHeader();
+    });
+});*/
+
