805f3b8e3e9acb6d11c0c4e37aca5550b4454d3f
Zender <zenderaurelien4@gmail.com>
2026-08-05 00:49:46 -0400

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,6 +8,29 @@ import { loadCart } from "../data/cart.js";
 //promises are a better alternative to callbacks as they allow for less nesting (both let you run async code)
 
 //promise.all lets you run multiple promises and wait for all to finish
+
+//async await is a shortcut for promises
+//async keyword makes a function return a promise
+//await waits for a promise to finish before going to next line
+async function loadPage(){
+    await loadProductsFetch(); //await basically replaces .then()
+    //closest function has to be async, so await cannot be nested in a regular function that is nested inside an async
+    const value = await new Promise((resolve) =>{
+        loadCart(() =>{
+            resolve('value3'); //saves resolve value to variable
+        });
+    })
+
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+
+    // returns convert to resolve(value)
+    //await can ONLY be used inside an async function
+}
+loadPage();
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) =>{
@@ -22,7 +45,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
-});
+}); */
 
 
 
